49fd1112c286cb21278c70b2532b60ea29e91e83
will <willgb10@hotmail.com>
2021-02-03 02:05:34 -0300

    Criando operacao de instaciacao

--- src/main/java/com/willsproject/cursomc/CursomcApplication.java
+++ src/main/java/com/willsproject/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.willsproject.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.willsproject.cursomc.domain.Categoria;
+import com.willsproject.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{//CommandLineRunner permite implementar o metodo auxiliar para executar uma acao quando a aplicacao comecar
+
+	@Autowired
+	private CategoriaRepository categoriaRepository; //reponsável de acessar os dados e salvar eles, manipular
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
+	
+	
 
 }
